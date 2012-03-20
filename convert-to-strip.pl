2bc31d172aeb85ea7e6cab97806f5a76e1a37af4
Billy Gray <wgray@zetetic.net>
2012-03-20 15:50:11 -0400

    working on 1password conversion to the app, still experiencing an error at the end of print_csv

--- convert-to-strip.pl
+++ convert-to-strip.pl
@@ -10,7 +10,8 @@ use Term::ReadKey;
 use Tkx;
 use Text::CSV;
 use Text::CSV_PP;
-#use Data::Dumper;
+use JSON;
+use Data::Dumper;
 
 use vars qw($opt_source $opt_target $file);
 
@@ -26,16 +27,27 @@ my $source_button = $frame->new_ttk__button(-text => 'Source File',  -command =>
 $source_button->g_grid(-row => 1, -column => 0, -sticky => 'nw', -padx => 10, -pady => 5);
 
 my $directory_entry = $frame->new_ttk__entry(-width => 60, -textvariable => \$opt_source);
-$directory_entry->g_grid(-row => 1, -column => 1, -sticky => 'nw', -padx => 10, -pady => 5);
+$directory_entry->g_grid(-row => 1, -column => 1, -sticky => 'nw', -padx => 10, -pady => 5, -columnspan => 2);
 
 my $file_button = $frame->new_ttk__button(-text => 'Save As',  -command => \&getTarget);
 $file_button->g_grid(-row => 2, -column => 0, -sticky => 'nw', -padx => 10, -pady => 5);
 
 my $file_entry = $frame->new_ttk__entry(-width => 60, -textvariable => \$opt_target);
-$file_entry->g_grid(-row => 2, -column => 1, -sticky => 'nw', -padx => 10, -pady => 5);
+$file_entry->g_grid(-row => 2, -column => 1, -sticky => 'nw', -padx => 10, -pady => 5, -columnspan => 2);
+
+my $format_label = $frame->new_ttk__label(-text => 'Source Format');
+$format_label->g_grid(-row => 3, -column => 0, -sticky => 'nw', -padx => 10, -pady => 5);
+
+my $source_format = 'splashidvid';
+my $radio_splashid = $frame->new_ttk__radiobutton(-text => "SplashID vID", -value => $source_format,
+                                           -variable=> \$source_format);
+$radio_splashid->g_grid(-row => 3, -column => 1, -sticky => 'nw', -padx => 10, -pady => 5);
+my $radio_1password = $frame->new_ttk__radiobutton(-text => "1Password .1pif", -value => "1password",
+                                             -variable=> \$source_format);
+$radio_1password->g_grid(-row => 3, -column => 2, -sticky => 'nw', -padx => 10, -pady => 5);
 
 my $export_button = $frame->new_ttk__button(-text => 'Run Conversion',  -command => \&export);
-$export_button->g_grid(-row => 3, -column => 1, -sticky => 'nw', -padx => 10, -pady => 5);
+$export_button->g_grid(-row => 4, -column => 1, -sticky => 'nw', -padx => 10, -pady => 5);
  
 Tkx::MainLoop();
 
@@ -64,11 +76,73 @@ sub validate {
 
 sub export {
   if(validate()) {
-    splashIdToStrip();
+    if ($source_format eq 'splashidvid') {
+      splashIdToStrip();
+    } 
+    else {
+      onePasswordToStrip();
+    }
     Tkx::tk___messageBox(-message => "Conversion complete!\n", -type => "ok");
   }
 }
 
+sub onePasswordToStrip {
+  my @entries = ();
+  my @fields  = ();
+  my $fh;
+  
+  unless(open($fh, "<", $opt_source)) {
+    Tkx::tk___messageBox(-message => "Can't open source file " . $opt_source . "!\n", -type => "ok");
+    return;
+  }
+  while(<$fh>) {
+    # There are odd noise lines between each entry, each the same:
+    # ***5642bee8-a5ff-11dc-8314-0800200c9a66***
+    # We'll just check for the first three asterisks and skip if detected.
+    if ($_ =~ /^\*\*\*.*$/) { next; }
+    my $row       = JSON->new->utf8->decode($_);
+    # typeName: wallet.financial.CreditCard, split and take the last array element
+    my @decimals  = split(/\./, $row->{'typeName'});
+    my $entry     = {
+      'name'      => $row->{'title'},
+      'category'  => $decimals[-1],
+      'fields'    => {}
+    };
+    my $idx = 0;
+    # secureContents is another hash, describing the entry fields,
+    # and most of these are empty, which is just delightful.
+    foreach(keys %{$row->{'secureContents'}}) {
+      my $name = $_;
+      my $value = $row->{'secureContents'}->{$name};
+      # add the field to our list if it's not in there already (but ignore the 'fields' name, holding an inner set of fields)
+      if(!contains($name, @fields) && $name ne 'fields') {
+         push(@fields, $name);
+      }
+      # add it to the entry if it's not blank
+      if (defined($value) && $value ne '') {
+        # if it's named 'fields' we need to go one level deeper
+        if ($name eq 'fields') {
+          foreach(@$value) {
+            # each element of the array is hash, we want the name and value keys
+            # see if this field name is in @fields...
+            if (!contains($_->{'name'}, @fields)) { push(@fields, $_->{'name'}); }
+            # if it has a value, add it to our set for the entry
+            if (defined($_->{'value'}) && $_->{'value'} ne '') {
+              $entry->{'fields'}->{$_->{'name'}} = $_->{'value'};
+            }
+          }
+        }
+        else {
+          $entry->{'fields'}->{$name} = $value;
+        }
+      }   
+    }
+    push(@entries, $entry);
+    $idx++;
+  }
+  close($fh);
+  print_csv(@entries, @fields);
+}
 
 sub splashIdToStrip {
   my $categories = {};
@@ -76,7 +150,6 @@ sub splashIdToStrip {
   my @fields = ();
   my $idx = 0;
   my $fh;
-
   my $csv = Text::CSV->new({binary => 1});
   
   unless(open($fh, "<", $opt_source)) {
@@ -88,7 +161,6 @@ sub splashIdToStrip {
 
        if($idx++ < 2) {next;};
        my @row = @$rowref;
-
 
        if($row[0] eq 'T') {
          # if the first column contains a T, it's a category definition, e.g.
@@ -124,19 +196,31 @@ sub splashIdToStrip {
        }
   }
   close($fh);
+  print_csv(@entries, @fields);
+}
 
+sub contains {
+  my $val = shift;
+  my %vals = map {$_=>1} @_;
+  return (exists($vals{$val})) ? 1 : 0;
+}
+
+# print_csv(@entries, @fields);
+sub print_csv {
+  my(@entries, @fields) = @_;
+  my $fh;
+  my $csv = Text::CSV->new({binary => 1});
+  
   @fields = sort(@fields);
-
   unless(open($fh, ">", $opt_target)) {
     Tkx::tk___messageBox(-message => "Can't open target file!\n", -type => "ok");
     return;
   }
-
   my @header = (("Category", "Entry"), @fields);
   $csv->print($fh, \@header);
-   
   foreach(@entries) {
     my $entry = $_;
+    print Dumper($entry);
     my @row = ($entry->{"category"}, $entry->{"name"});
     foreach(@fields) {
       if(exists($entry->{"fields"}->{$_})) {
@@ -147,12 +231,5 @@ sub splashIdToStrip {
     } 
     $csv->print($fh, \@row);
   }
-
   close($fh);
-}
-
-sub contains {
-  my $val = shift;
-  my %vals = map {$_=>1} @_;
-  return (exists($vals{$val})) ? 1 : 0;
 }
