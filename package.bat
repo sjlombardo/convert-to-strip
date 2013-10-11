3a3ad55aed816b13544059fbdc1fc5a4a48f6eab
Billy Gray <wgray@zetetic.net>
2013-10-11 10:14:41 -0400

    attempts to build perlapp while specifying both 32 and 64 bit archs for windows version

--- package.bat
+++ package.bat
@@ -1,7 +1,7 @@
 set EXE=convert-to-strip.exe
 set APP=convert-to-strip.app
 
-perlapp --trim JSON::PP58 --norunlib --gui --nologo --info "CompanyName=Zetetic LLC;FileVersion=1.0.1;InternalName=convert-to-strip;LegalCopyright=Copyright, 2013 all rights reserved;OriginalFilename=convert-to-strip;ProductName=Convert To Strip;ProductVersion=1.0.1" --exe %EXE% convert-to-strip.pl
+perlapp --trim JSON::PP58 --norunlib --gui --nologo --target windows-x86-32 --info "CompanyName=Zetetic LLC;FileVersion=1.0.1;InternalName=convert-to-strip;LegalCopyright=Copyright, 2013 all rights reserved;OriginalFilename=convert-to-strip;ProductName=Convert To Strip;ProductVersion=1.0.1" --exe %EXE% convert-to-strip.pl
 
 perlapp --trim JSON::PP58 --norunlib --gui --nologo --target macosx-universal-32 --exe %APP% convert-to-strip.pl
 
