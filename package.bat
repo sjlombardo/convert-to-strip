f7d0ac2802d805522ec3b3f71c3f0275373d86d4
Billy Gray <wgray@zetetic.net>
2014-11-14 16:45:08 -0500

    Update version number specified in package.bat

--- package.bat
+++ package.bat
@@ -2,7 +2,7 @@ set EXE=convert-to-strip.exe
 set APP=convert-to-strip.app
 set LNX=convert-to-strip-linux
 
-perlapp --trim JSON::PP58 --norunlib --gui --nologo --target windows-x86-32 --info "CompanyName=Zetetic LLC;FileVersion=1.0.3;InternalName=convert-to-strip;LegalCopyright=Copyright 2012-2013 All rights reserved;OriginalFilename=convert-to-strip;ProductName=Convert to STRIP;ProductVersion=1.0.3" --exe %EXE% convert-to-strip.pl
+perlapp --trim JSON::PP58 --norunlib --gui --nologo --target windows-x86-32 --info "CompanyName=Zetetic LLC;FileVersion=1.0.4;InternalName=convert-to-strip;LegalCopyright=Copyright 2012-2013 All rights reserved;OriginalFilename=convert-to-strip;ProductName=Convert to STRIP;ProductVersion=1.0.4" --exe %EXE% convert-to-strip.pl
 
 perlapp --trim JSON::PP58 --norunlib --gui --nologo --target macosx-universal-32 --exe %APP% convert-to-strip.pl
 
