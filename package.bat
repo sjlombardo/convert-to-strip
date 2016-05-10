75a6c3d426c126b20012f736f78fb1f77545aa8e
Billy Gray <wgray@zetetic.net>
2016-05-10 11:10:31 -0400

    Sets version string to 1.0.6

--- package.bat
+++ package.bat
@@ -2,7 +2,7 @@ set EXE=convert-to-codebook.exe
 set APP=convert-to-codebook.app
 set LNX=convert-to-codebook-linux
 
-perlapp --trim JSON::PP58 --norunlib --gui --nologo --target windows-x86-32 --info "CompanyName=Zetetic LLC;FileVersion=1.0.4;InternalName=convert-to-codebook;LegalCopyright=Copyright 2012-2013 All rights reserved;OriginalFilename=convert-to-codebook;ProductName=Convert to Codebook;ProductVersion=1.0.4" --exe %EXE% convert-to-codebook.pl
+perlapp --trim JSON::PP58 --norunlib --gui --nologo --target windows-x86-32 --info "CompanyName=Zetetic LLC;FileVersion=1.0.6;InternalName=convert-to-codebook;LegalCopyright=Copyright 2012-2016 Zetetic LLC All rights reserved;OriginalFilename=convert-to-codebook;ProductName=Convert to Codebook;ProductVersion=1.0.6" --exe %EXE% convert-to-codebook.pl
 
 perlapp --trim JSON::PP58 --norunlib --gui --nologo --target macosx-universal-32 --exe %APP% convert-to-codebook.pl
 
